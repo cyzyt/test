9100cbb5846eaefec01bbb667453247b2171e818
chenyu <447469779@qq.com>
2025-01-14 11:35:18 +0800

    table用法备注

--- chuanqi/MirServer/Mir200/Envir/Extension/LuaLibrary/table.lua
+++ chuanqi/MirServer/Mir200/Envir/Extension/LuaLibrary/table.lua
@@ -23,9 +23,9 @@ function table.nums(t)
     return count
 end
 
--- 获取表的所有索引key的集合
----*  hashtable : table表
----*  @return : 新的数组表，存储hashtable中所有的key
+-- 获取哈希表中所有的键，并存储在一个新的表中
+---*  hashtable : 哈希表
+---*  @return : 存储了所有键的 keys 表作为结果返回
 ---@param hashtable table
 ---@return table
 function table.keys(hashtable)
@@ -36,9 +36,9 @@ function table.keys(hashtable)
     return keys
 end
 
--- 获取表的所有值value的集合
----*  hashtable : table表
----*  @return : 新的数组表，存储hashtable中所有的value
+-- 获取哈希表中所有的值，并存储在一个新的表中
+---*  hashtable : 哈希表
+---*  @return : 存储了所有值的 values 表作为结果返回
 ---@param hashtable table
 ---@return table
 function table.values(hashtable)
@@ -49,7 +49,8 @@ function table.values(hashtable)
     return values
 end
 
--- 表合并表
+-- 将 src 表中的元素合并到 dest 表中（在合并过程中，可能会修改 dest 表的内容）
+-- 如果 dest 表中已经存在该键，则该键的值将被 src 表中对应的值覆盖；如果不存在，则会新增这个键值对。
 ---*  dest : 合并表
 ---*  src : 被合并表
 ---@param dest table
@@ -60,9 +61,9 @@ function table.merge(dest, src)
     end
 end
 
--- 表插入表
----*  dest : 插入表
----*  src : 被插入表
+-- 将一个源表 src 的元素插入到目标表 dest 中，从指定的起始位置 begin 开始插入。
+---*  dest : 目标表源表
+---*  src : 源表
 ---*  begin : 插入位置，为空时默认插入在表dest的开头，小于零默认插入在结尾
 ---@param dest table
 ---@param src table
@@ -79,11 +80,11 @@ function table.insertto(dest, src, begin)
     end
 end
 
--- 根据value值查找其在数组中的序列号
+-- 根据value值查找其在数组中的索引位置
 ---*  array : 被查找表（数组结构、列表式）
----*  value : 查找的目标
----*  begin : 查找的起始位置，为空则默认从开头开始查找；
----*  @return : 查找成功返回序列号，int类型；失败返回false，bool类型
+---*  value : 需要查找的元素值
+---*  begin : 开始查找的起始位置，默认为 1
+---*  @return : 查找成功返回索引位置，int类型；失败返回false
 ---@param array table
 ---@param value integer|string
 ---@param begin integer
@@ -95,10 +96,10 @@ function table.indexof(array, value, begin)
     return false
 end
 
--- 根据value值查找其在表的key
----*  hashtable : 被查找表（记录式）
----*  value : 查找的目标
----*  @return : 查找成功返回key，string类型；失败返回nil
+-- 根据value值查找其在哈希表中对应的键key
+---*  hashtable : 要查找元素的哈希表
+---*  value : 需要查找的元素的值
+---*  @return : 如果遍历完整个哈希表都没有找到满足条件的元素，函数将返回 nil
 ---@param hashtable table
 ---@param value integer|string
 ---@return string|nil
@@ -109,11 +110,11 @@ function table.keyof(hashtable, value)
     return nil
 end
 
--- 根据value值，将其从数组中移除
----*  array : 目标表（数组结构、列表式）
----*  value : 需要移除的值
----*  removeall : true全部移除，false移除一个
----*  @return : 移除次数
+-- 从 array 表中移除指定的 value
+---*  array : 要操作的表（数组结构、列表式）
+---*  value : 要从表中移除的值
+---*  removeall : 是一个布尔值，用于控制是否移除表中所有等于 value 的元素；true全部移除，false移除一个
+---*  @return : 移除元素的数量
 ---@param array table
 ---@param value integer|string
 ---@param removeall boolean
@@ -133,7 +134,7 @@ function table.removebyvalue(array, value, removeall)
     return c
 end
 
--- 对表按条件进行重新赋值
+-- 对表格 t 中的每个元素进行映射操作
 ---*  t : 目标表
 ---*  fn : 条件函数
 ---@param t table
@@ -144,7 +145,7 @@ function table.map(t, fn)
     end
 end
 
--- 对表遍历并将key,value代入函数中执行函数
+-- 对表遍历并对表格中的每个元素执行指定的操作
 ---*  t : 目标表
 ---*  fn : 执行函数
 ---@param t table
@@ -168,7 +169,7 @@ end
 
 -- 对表的value去重
 ---*  t : 目标表
----*  bArray : 去重后的新表索引是否按数组格式，true为按数组格式，false按原目标表中的key为索引
+---*  bArray : 用于控制存储去重元素的方式，true为按数组格式，false按原表中的键 k 作为键
 ---*  @return : 对表的value去重后新的表
 ---@param t table
 ---@param bArray boolean
@@ -191,11 +192,11 @@ function table.unique(t, bArray)
     return n
 end
 
--- 通过key,value找出对应的嵌套表
+-- 通过key,value查找符合条件的元素
 ---*  t : 目标表
----*  key : key
----*  value : value
----*  @return : 对表的value去重后新的表
+---*  key : 是一个键，用于指定要查找的元素的某个属性
+---*  value : 是一个值，用于指定要查找的元素的某个属性的值
+---*  @return : 符合条件的元素或者nil
 ---@param t table
 ---@param key string|integer
 ---@param value any
@@ -209,6 +210,13 @@ function table.findtbykv(t, key, value)
     return nil
 end
 
+-- 自定义的表格查找(用于在表格 t 中查找满足某个条件的值)
+---*  t : 目标表
+---*  fn : 查找条件（该函数接收表格元素 t[i] 和索引 i 作为参数，需要根据元素和索引来判断是否满足查找条件。）
+---*  reverse : 用于决定查找的方向，true 表示从后往前查找，false 表示从前往后查找
+---@param t table
+---@param fn function
+---@param reverse boolean
 function table.findarrvalue(t, fn, reverse)
     local size = #t
     local begin = reverse and size or 1
